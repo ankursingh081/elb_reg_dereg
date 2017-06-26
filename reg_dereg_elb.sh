803fe29492b0d13d66b24d43069e22e0f1bcad80
Ankur Singh <ankursingh081@gmail.com>
2017-06-26 17:33:16 +0530

    final

--- reg_dereg_elb.sh
+++ reg_dereg_elb.sh
@@ -23,110 +23,113 @@ which jq >/dev/null || {
 }
 
 case "$1" in
-    --put-metrics )
-        loadData;
-        ;;
     --perform )
         perform;
-        ;;
-    --show )
-        loadData;
         ;;
     *)
         usage;
         ;;
 esac
 }
-waitUntil () {
-            echo -n "Wait until state is $1"
+waitUntil ()
+{ 
+           echo -n "Wait until state is $1"
             while [ "$(getState)" != "$1" ]; do
                 echo -n "."
                 sleep 1
             done
         echo
 	}
-        
-perform() {
-    echo "1. Add Instance"
-    echo "2. Remove Instance"
-    read input
     
-    echo "Enter Load Balancer  Name"
-    aws elb describe-load-balancers | jq -r '.LoadBalancerDescriptions[].LoadBalancerName'
-    read lbname
-
-
-    if [ $input==1 ]; then
-
-echo "Dispatcher instances inside the selected Load Balancer"
-        aws elb describe-load-balancers --load-balancer-name $lbname | jq -r '.LoadBalancerDescriptions[].Instances[].InstanceId'
-echo
-	echo "Provide the instance-id Of Instance to Add"
-        
-        aws ec2 describe-instances |jq -r '.Reservations[].Instances[] | [.InstanceId, .ClientToken]| @json'
-        
-        listinstance=`aws ec2 describe-instances |jq -r '.Reservations[].Instances[].InstanceId'`
-      
- echo $listinstance 
-        while read instanceids
-        do
-		for i in ${listinstance[@]}
-		do
-			echo $i
-        		if [ $instanceids != $i ]; then
-        		echo "Instance id is innorrect.. Try Again.."
-       	 		fi
-		done
-break
-    	done
-    echo "Checking Status of the instance"
-
-        getState () {
+    
+      getState () {
             aws elb describe-instance-health \
                 --load-balancer-name $lbname \
-                --instance $instanceids | jq '.InstanceStates[].State' -r
+                --instance $instanceids | jq '.InstanceStates[0].State' -r
         }
-echo "Registering"
         register () {
             aws elb register-instances-with-load-balancer \
                 --load-balancer-name $lbname \
                 --instance $instanceids | jq .
         }
-        echo "De-Registering"
          deregister () {
         aws elb deregister-instances-from-load-balancer \
             --load-balancer-name $lbname \
             --instance $instanceids | jq .
         }
+        
+perform() {
+echo "Enter Region"
+read region
+export AWS_DEFAULT_REGION="$region"
+    echo "1. Add Instance"
+    echo "2. Remove Instance"
+    read input
+    echo "Enter Load Balancer Name"
+    aws elb describe-load-balancers | jq -r '.LoadBalancerDescriptions[].LoadBalancerName'
+    echo
+    read lbname
 
-    elif [ $input==2 ]; then
-    
-        echo "Instance in selected ELB"
-        aws elb describe-load-balancers --load-balancer-name $lbname | jq -r '.LoadBalancerDescriptions[].Instances[].Instanceid'
-        echo "Provide the Instance-Id"
-        read $InstanceID
-    
-    
-	fi
-        
-        
-    
-        if [ "$(getState)" == "OutOfService" ]; then
-            register >> /dev/null
+    if [ $input == "1" ]; then
+
+echo "Dispatcher instances inside the selected Load Balancer"
+echo
+disp_list=`aws elb describe-load-balancers --load-balancer-name $lbname | jq -r '.LoadBalancerDescriptions[].Instances[].InstanceId'`
+if [ "$disp_list" == " " ]; then
+echo "Load Balancer is Empty"
+else
+aws elb describe-load-balancers --load-balancer-name $lbname | jq -r '.LoadBalancerDescriptions[].Instances[].InstanceId'
+fi
+echo
+	echo "Provide the instance-id Of Instance to Add"
+    echo    
+        aws ec2 describe-instances |jq -r '.Reservations[].Instances[] | [.InstanceId, .ClientToken]| @json'
+        echo
+listinstance=`aws ec2 describe-instances | jq -r '.Reservations[].Instances[].InstanceId'`
+while read -r instanceids;
+do        
+echo "$listinstance" | grep -Fxe "$instanceids"
+if [ "$?" == "0" ]; then
+break
+else
+echo "Instance not available... Try Again "
+fi
+done
+echo "Registering Instance $instanceids" 
+register >>/dev/null
+sleep 1
+echo "Checking Status of the instance"
+echo
+#echo "Instance ${instanceids} is $(getstatee)"
+       if [ "$(getState)" == "OutOfService" ]; then
+waitUntil "InService"
+aws elb describe-instance-health --load-balancer-name $lbname | jq -r '["       ID","        State"], ["     --------","        ------"], (.InstanceStates[]|[.InstanceId, .State]) | @tsv'
+       elif [ "$(getState)" == "InService" ]; then
+            echo "Instance inside the Load Balancer $lbname"
+echo "Instance status inside ELB" 
+aws elb describe-instance-health --load-balancer-name $lbname | jq -r '["       ID","        State"], ["     --------","        ------"], (.InstanceStates[]|[.InstanceId, .State]) | @tsv'
+
+            exit 1
         fi
+    elif [ $input == "2" ]; then
+        echo "Instance in selected ELB"
+        aws elb describe-load-balancers --load-balancer-name $lbname | jq -r '.LoadBalancerDescriptions[].Instances[].InstanceId'
+        echo "Provide the Instance-Id"
+        read instanceids
+   
+   
+   echo "Instance ${instanceids} is $(getState)"
+echo "Removing"
 
-        waitUntil "InService"
-
-        lburl=`aws elb describe-load-balancers --load-balancer-name $lbname | jq -r '.LoadBalancerDescriptions[].DNSName'`
-    
-        curl $lburl &
-        sleep 1
-        
-    deregister >> /dev/null
-
-    waitUntil "OutOfService"
-    
-    
+        if [ "$(getState)" == "OutOfService" ]; then
+            deregister >>/dev/null
+      elif [ "$(getState)" == "InService" ]; then
+            deregister >> /dev/null
+            fi
+echo "Instance status inside ELB " 
+aws elb describe-instance-health --load-balancer-name $lbname | jq -r '["       ID","        State"], ["     --------","        ------"], (.InstanceStates[]|[.InstanceId, .State]) | @tsv'
+	else
+exit
+fi
 }
-
 main "$@"
--- test1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-aws elb describe-instance-health --load-balancer-name ankur-50-test | jq -r '["       ID","        State"], ["     --------","        ------"], (.InstanceStates[]|[.InstanceId, .State]) | @tsv'
